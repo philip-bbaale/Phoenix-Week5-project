856011d4cf6ed43a9c7e97760282422fc5b84b2a
makenzie-ops <laurenmakenzie0@gmail.com>
2020-03-05 09:15:45 +0300

    Add form

--- js/anemia.js
+++ js/anemia.js
@@ -3,6 +3,7 @@ function timetable ( day , date ,food) {
     this.date = date;
     this.food = food;
 }
+
 $(document).ready(function(){
    $("#see").click(function(event){
     event.preventDefault();
